8d46394170b46a9c8555771812a12fdf56409736
Mariia Malitska <37335222+MariiaMalitska@users.noreply.github.com>
2019-10-05 23:22:09 +0300

    Initial commit

---
+++ meetuply-server/src/main/java/com/meetuply/meetuply/MeetuplyApplication.java
@@ -0,0 +1,13 @@
+package com.meetuply.meetuply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetuplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetuplyApplication.class, args);
+	}
+
+}
